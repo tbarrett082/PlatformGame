6191be5998a81c0253aa50cb09ea9f6abc788571
Tanner <tannerB@Janines-MacBook-Pro.local>
2022-02-04 01:04:19 -0700

    Sloped roads suck

--- src/characters/MajorBrainer.js
+++ src/characters/MajorBrainer.js
@@ -6,19 +6,20 @@ export default class extends Phaser.Physics.Arcade.Sprite {
         this.scene = scene;
         this.fireRate = 500;
         this.nextShot = 0;
-        this.walking = false;
 
         // Add to game and physics
         scene.add.existing(this);
         scene.physics.add.existing(this);
         this.body.setCollideWorldBounds();
 
-        // Set the hitbox size
-        this.body.setSize(this.width/this.scale.x, this.height/this.scale.y);
-
         // Collisions
         scene.physics.add.collider(this, scene.platforms);
         scene.physics.add.collider(this, scene.building3Platforms);
+        scene.physics.add.collider(this, scene.invisiLayerPlatforms);
+        scene.physics.add.collider(this, scene.roadFlat1);
+        scene.physics.add.collider(this, scene.roadFlat2);
+        scene.physics.add.collider(this, scene.roadFlat3);
+        scene.physics.add.collider(this, scene.roadFlat4);
     }
     preUpdate(time, delta) {
         super.preUpdate(time, delta);
@@ -57,6 +58,86 @@ export default class extends Phaser.Physics.Arcade.Sprite {
             }
         }
 
+        /**
+         * Overlaps with street slopes
+         */
+        if (this.scene.physics.overlap(this, this.scene.roadSlope)) {
+            if (this._intersectsSlopeTriangle(this.getBounds(), this.scene.slopeTriangle1)) {
+                this.body.blocked.down = true;
+                if (this.scene.cursors.up.isDown) {
+                    this.jump();
+                } else {
+                    var x1 = this.scene.roadSlope.body.position.x;
+                    console.log(x1);
+                    var x2 = x1 + this.scene.roadSlope.width;
+                    var y2 = this.scene.roadSlope.body.position.y;
+                    var y1 = y2 + this.scene.roadSlope.body.height;
+
+                    this.body.y = this._getSlopePositionY(x1, x2, y1, y2);
+                    this.body.setAllowGravity(false);
+                }
+            }
+        }
+        else {
+            this.body.setAllowGravity(true);
+        }
+        if (this.scene.physics.overlap(this, this.scene.roadSlope2)) {
+            if (this._intersectsSlopeTriangle(this.getBounds(), this.scene.slopeTriangle2)) {
+                this.body.blocked.down = true;
+                if (this.scene.cursors.up.isDown) {
+                    this.jump();
+                } else {
+                    var x1 = this.scene.roadSlope2.body.position.x;
+                    var x2 = x1 + this.scene.roadSlope2.width;
+                    var y2 = this.scene.roadSlope2.body.position.y;
+                    var y1 = y2 + this.scene.roadSlope2.body.height;
+
+                    this.body.y = this._getSlopePositionY(x1, x2, y1, y2);
+                    this.body.setAllowGravity(false);
+                }
+            }
+        } else {
+            this.body.setAllowGravity(true);
+        }
+
+        if (this.scene.physics.overlap(this, this.scene.roadSlope3)) {
+            if (this._intersectsSlopeTriangle(this.getBounds(), this.scene.slopeTriangle3)) {
+                this.body.blocked.down = true;
+                if (this.scene.cursors.up.isDown) {
+                    this.jump();
+                } else {
+                    var x1 = this.scene.roadSlope3.body.position.x;
+                    var x2 = x1 + this.scene.roadSlope3.width;
+                    var y2 = this.scene.roadSlope3.body.position.y;
+                    var y1 = y2 + this.scene.roadSlope3.body.height;
+
+                    this.body.y = this._getSlopePositionY(x1, x2, y1, y2);
+                    this.body.setAllowGravity(false);
+                }
+            }
+        } else {
+            this.body.setAllowGravity(true);
+        }
+
+        if (this.scene.physics.overlap(this, this.scene.roadSlope4)) {
+            if (this._intersectsSlopeTriangle(this.getBounds(), this.scene.slopeTriangle4)) {
+                this.body.blocked.down = true;
+                if (this.scene.cursors.up.isDown) {
+                    this.jump();
+                } else {
+                    var x1 = this.scene.roadSlope4.body.position.x;
+                    var x2 = x1 + this.scene.roadSlope4.width;
+                    var y2 = this.scene.roadSlope4.body.position.y;
+                    var y1 = y2 + this.scene.roadSlope4.body.height;
+
+                    this.body.y = this._getSlopePositionY(x1, x2, y1, y2);
+                    this.body.setAllowGravity(false);
+                }
+            }
+        } else {
+            this.body.setAllowGravity(true);
+        }
+
         return this.body.position.x;
     }
 
@@ -64,5 +145,25 @@ export default class extends Phaser.Physics.Arcade.Sprite {
         if (this.body.blocked.down) {
             this.setVelocityY(-400);
         }
+    }
+
+    _getSlopePositionY(x1, x2, y1, y2) {
+        var width = x2 - x1;
+        var height = y2 - y1;
+        var playerRelX;
+        if (this.body.position.x < x1) {
+            return this.body.position.y;
+        } else {
+            playerRelX = this.body.position.x - x1;
+        }
+        var percent = playerRelX / width;
+        var heightPercent = percent * height;
+        var finalHeight = heightPercent + y1 - (this.body.height);
+
+        return finalHeight;
+    }
+
+    _intersectsSlopeTriangle(objA, objB) {
+        return Phaser.Geom.Intersects.RectangleToTriangle(objA, objB);
     }
 }
--- src/scenes/BrainvilleScene.js
+++ src/scenes/BrainvilleScene.js
@@ -12,7 +12,6 @@ export default class BrainvilleScene extends Phaser.Scene {
     constructor() {
         super();
         this.shotCount = 0;
-        this.walking = false;
     }
 
     preload() {
@@ -33,6 +32,8 @@ export default class BrainvilleScene extends Phaser.Scene {
         this.load.image('concrete-wall-tiles', 'assets/tilesets/concrete-wall.png');
         this.load.image('bush-1-tiles', 'assets/tilesets/bushes-1.png');
         this.load.image('building-3-tiles', 'assets/tilesets/building-3-tiles.png');
+        this.load.image('building-4-tiles', 'assets/tilesets/building-4.png');
+        this.load.image('invisi-layer-tiles', 'assets/tilesets/invisi-layer.png');
 
         //Load Json for initial stat
         this.load.json('world-init', 'data/world-load-01.json');
@@ -45,12 +46,20 @@ export default class BrainvilleScene extends Phaser.Scene {
         this.load.image('bullet', 'assets/images/bullet.png');
         this.load.image('nsx', 'assets/images/nsx.png');
 
+        // Load Street Slope sprites
+        this.load.image('road-flat-1', 'assets/images/road-flat-1.png');
+        this.load.image('road-flat-2', 'assets/images/road-slope/road-flat-2.png');
+        this.load.image('road-flat-3', 'assets/images/road-slope/road-flat-3.png');
+        this.load.image('road-flat-4', 'assets/images/road-slope/road-flat-4.png');
+        this.load.image('road-slope', 'assets/images/road-slope.png');
+        this.load.image('road-slope-2', 'assets/images/road-slope-2.png');
+        this.load.image('road-slope-3', 'assets/images/road-slope/road-slope-3.png');
+        this.load.image('road-slope-4', 'assets/images/road-slope/road-slope-4.png');
+
         //Load Audio
         this.load.audio('welcome-to-brainville', 'assets/audio/Welcome to brainville.wav');
         this.load.audio('welcome-to-china-town', 'assets/audio/welcome to china town 2.wav');
         this.load.audio('glock-shot', 'assets/audio/Glock-2.wav');
-        this.load.audio('walking', 'assets/audio/footsteps.wav')
-
     }
 
     create() {
@@ -61,36 +70,38 @@ export default class BrainvilleScene extends Phaser.Scene {
         let bg = this.add.image(0, 0, 'background').setScrollFactor(0).setOrigin(0, 0);
         this.add.image(2000, 0, 'building-3-inside').setOrigin(0, 0).setScrollFactor(0.5, 1);
         let tcb = this.add.image(0, 1200, 'tree-city-background').setOrigin(0, 0).setScrollFactor(0.5, 1);
-        // let bridge = this.add.image(3050, 250, 'bridge').setScale(4);
         this.cameras.main.setBounds(0, 0, 15000, 1800);
-        
 
         /**
          * Tilemap Data
          */
         var platformMap = this.make.tilemap({ key: 'street-map' });
         var concreteTileset = platformMap.addTilesetImage('tileset-street', 'street-tiles');
-        var buildingTileset = platformMap.addTilesetImage('building-1', 'middleground-tiles');
         var wallTileset = platformMap.addTilesetImage('concrete-wall', 'concrete-wall-tiles');
         var bushTileset = platformMap.addTilesetImage('bushes-1', 'bush-1-tiles');
         var building3Tileset = platformMap.addTilesetImage('building-3', 'building-3-tiles');
+        var invisiLayerTileset = platformMap.addTilesetImage('invisi-layer', 'invisi-layer-tiles');
 
         /**
          * Tilemap creation
          */
         var streetPositionYPlatform = this.physics.world.bounds.bottom - (platformMap.height * platformMap.tileHeight);
-        console.log(streetPositionYPlatform);
+
 
         // create middleground layers
-        platformMap.createLayer('Building', buildingTileset, 0, streetPositionYPlatform - 237);
         platformMap.createLayer('concrete-wall', wallTileset, 0, streetPositionYPlatform - 16);
         platformMap.createLayer('trees-front', bushTileset, 0, streetPositionYPlatform - 48);
+
 
         //Create street
         this.platforms = platformMap.createLayer('Street', concreteTileset, 0, streetPositionYPlatform);
         this.platforms.setCollisionByExclusion(-1, true);
-        this.building3Platforms = platformMap.createLayer('building-3', building3Tileset, 0, streetPositionYPlatform -16).setScrollFactor(0.5, 1);
+        this.building3Platforms = platformMap.createLayer('building-3', building3Tileset, 0, streetPositionYPlatform - 16).setScrollFactor(0.5, 1);
         this.building3Platforms.setCollisionByExclusion(-1, true);
+        this.invisiLayerPlatforms = platformMap.createLayer('invisi-layer', invisiLayerTileset, 0, streetPositionYPlatform - 16);
+        this.invisiLayerPlatforms.setCollisionByExclusion(-1, true);
+
+
 
         // Generate animations
         var createAnims = createAnimations.bind(this);
@@ -99,9 +110,8 @@ export default class BrainvilleScene extends Phaser.Scene {
         /**
          * Create audio
          */
-        var brainvilleSong = this.sound.add('welcome-to-brainville');
-        // brainvilleSong.play();
-        this.walkingSound = this.sound.add('walking', {loop: true});
+        var brainvilleSong = this.sound.add('welcome-to-brainville', { loop: true, seek: 13.689 });
+        // brainvilleSong.play({seek: 13.689});
 
 
         // Load the keyboard keys
@@ -124,24 +134,104 @@ export default class BrainvilleScene extends Phaser.Scene {
     }
 
     _loadWorld(data) {
-        /**
-         * Decorations
-         */
+
+        this.add.image(data.car.x, data.car.y, 'nsx').setScale(0.3).setOrigin(0, 0);
+        this._loadStreetSlope(this);
+
+        //Create the player
         this.major = new MajorBrainer(this, data.major.x, data.major.y, 'major');
         this.cameras.main.startFollow(this.major);
-
-        //Image load
-        this.add.image(data.car.x, data.car.y, 'nsx').setScale(0.35).setOrigin(0, 0);
 
         /** 
          * Mini Brains
          */
         var miniBrainPositions = data.miniBrains;
         this.miniBrains.init(miniBrainPositions.length);
-        miniBrainPositions.forEach(function (obj) {this.miniBrains.startMiniBrain(obj.x, obj.y)}, this);
+        miniBrainPositions.forEach(function (obj) { this.miniBrains.startMiniBrain(obj.x, obj.y) }, this);
     }
 
-    _loadMiniBrains(data) {
+    _loadStreetSlope() {
+        this.roadSlope = this.physics.add.sprite(6000, 1700, 'road-slope').setOrigin(0, 0);
+        this.roadSlope.body.setCollideWorldBounds(true);
+        this.roadSlope.body.setAllowGravity(false);
+        this.roadSlope.body.setImmovable(true);
+        this.slopeTriangle1 = new Phaser.Geom.Triangle(
+            6000,
+            1800,
+            6000 + this.roadSlope.width,
+            1800,
+            6000 + this.roadSlope.width,
+            1800 - this.roadSlope.height);
+
+        this.graphics = this.add.graphics({
+            lineStyle: { width: 2, color: 0x00aa00 },
+            fillStyle: { color: 0xFFFFFF }
+        });
+
+        this.graphics.visible = true;
+        this.graphics.strokeTriangleShape(this.slopeTriangle1);
+
+        this.roadFlat1 = this.physics.add.sprite(6589, 1660, 'road-flat-1').setOrigin(0, 0);
+        this.roadFlat1.body.setImmovable(true);
+        this.roadFlat1.body.setAllowGravity(false);
+        this.roadFlat1.body.checkCollision.left = false;
+        this.roadFlat1.body.checkCollision.right = false;
+
+        this.roadSlope2 = this.physics.add.sprite(6840, 1553, 'road-slope-2').setOrigin(0, 0);
+        this.roadSlope2.body.setImmovable(true);
+        this.roadSlope2.body.setAllowGravity(false);
+        this.slopeTriangle2 = new Phaser.Geom.Triangle(
+            6840,
+            1553 + this.roadSlope2.height,
+            6840 + this.roadSlope2.width,
+            1553 + this.roadSlope2.height,
+            6840 + this.roadSlope2.width,
+            1553);
+            this.graphics.strokeTriangleShape(this.slopeTriangle2);
+
+        this.roadFlat2 = this.physics.add.sprite(7108, 1552, 'road-flat-2').setOrigin(0, 0);
+        this.roadFlat2.body.setImmovable(true);
+        this.roadFlat2.body.setAllowGravity(false);
+        this.roadFlat2.body.checkCollision.left = false;
+        this.roadFlat2.body.checkCollision.right = false;
+
+        this.roadSlope3 = this.physics.add.sprite(7184, 1464, 'road-slope-3').setOrigin(0, 0);
+        this.roadSlope3.body.setImmovable(true);
+        this.roadSlope3.body.setAllowGravity(false);
+        this.slopeTriangle3 = new Phaser.Geom.Triangle(
+            7184,
+            1464 + this.roadSlope3.height,
+            7184 + this.roadSlope3.width,
+            1464 + this.roadSlope3.height,
+            7184 + this.roadSlope3.width,
+            1464);
+            this.graphics.strokeTriangleShape(this.slopeTriangle3);
+
+        this.roadFlat3 = this.physics.add.sprite(7564, 1462, 'road-flat-3').setOrigin(0, 0);
+        this.roadFlat3.body.setImmovable(true);
+        this.roadFlat3.body.setAllowGravity(false);
+        this.roadFlat3.body.checkCollision.left = false;
+        this.roadFlat3.body.checkCollision.right = false;
+
+        this.roadSlope4 = this.physics.add.sprite(7803, 1344, 'road-slope-4').setOrigin(0, 0);
+        this.roadSlope4.body.setImmovable(true);
+        this.roadSlope4.body.setAllowGravity(false);
+        this.slopeTriangle4 = new Phaser.Geom.Triangle(
+            7803,
+            1344 + this.roadSlope4.height,
+            7803 + this.roadSlope4.width,
+            1344 + this.roadSlope4.height,
+            7803 + this.roadSlope4.width,
+            1344);
+            this.graphics.strokeTriangleShape(this.slopeTriangle4);
+
+        this.roadFlat4 = this.physics.add.sprite(8086, 1343, 'road-flat-4').setOrigin(0, 0);
+        this.roadFlat4.body.setImmovable(true);
+        this.roadFlat4.body.setAllowGravity(false);
+        this.roadFlat4.body.checkCollision.left = false;
+        this.roadFlat4.body.checkCollision.right = false;
+
+        this.graphics.clear();
 
     }
 
@@ -155,14 +245,6 @@ export default class BrainvilleScene extends Phaser.Scene {
             position = this.major.move(1);
         } else {
             position = this.major.move(0);
-        }
-
-        if (this.walking === false && this.cursors.right.isDown) {
-            this.walkingSound.play();
-            this.walking = true;
-        } else {
-            // this.walkingSound.stop();
-            this.walking = false;
         }
 
         // Set Up key for jump
